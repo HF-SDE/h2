249e5ae9094a79f5d4af5b8f4d2a40a07d5ae2a4
Philip Guldborg <phgu03@gmail.com>
2023-10-19 11:28:33 +0200

    Removed old stuff

--- FiveWordFiveLetters/FiveWordFiveLetters/GetWords.cs
+++ FiveWordFiveLetters/FiveWordFiveLetters/GetWords.cs
@@ -2,33 +2,6 @@
 {
     public class GetWords
     {
-        public static List<string> Words(string file)
-        {
-            try
-            {
-                String line;
-                List<string> words = new();
-                //Pass the file path and file name to the StreamReader constructor
-                StreamReader sr = new(file);
-                //Read the first line of text
-                line = sr.ReadLine()!;
-                //Continue to read until you reach end of file
-                while (line != null)
-                {
-                    //write the line to console window
-                    words.Add(line);
-                    //Read the next line
-                    line = sr.ReadLine()!;
-                }
-                //close the file
-                sr.Close();
-                return words;
-            }
-            catch
-            {
-                return new List<string>();
-            };
-        }
         public static string[] GetWord(string file, int wordLength = 5)
         {
             List<string> words = new();
