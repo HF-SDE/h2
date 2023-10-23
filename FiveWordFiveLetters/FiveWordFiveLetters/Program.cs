8f5ed72a44aed122b6e163adcadc7788060e06b7
Philip Guldborg <phgu03@gmail.com>
2023-10-23 13:09:06 +0200

    Optimiced

--- FiveWordFiveLetters/FiveWordFiveLetters/Program.cs
+++ FiveWordFiveLetters/FiveWordFiveLetters/Program.cs
@@ -1,97 +1,78 @@
 ﻿using FiveWordFiveLettersBLL;
 
-// Define
-//string[] perfectData = GetWords.GetWord("PerfectData.txt");
-
-// Test Collections
-//RunTest.Run(perfectData);
-
-//Console.WriteLine($"[Binary] Amount of perfect data: {Algoritme.Binary(perfectData).Count}");
-//Console.WriteLine($"[Word] Amount of perfect data: {Algoritme.NotOptimised(perfectData).Count}");
-//string[] notPerfectData = GetWords.GetWord("NotPerfectData.txt");
-//Console.WriteLine($"Amount of not perfect data: {Algoritme.NotOptimised(notPerfectData).Count}");
-//string[] beta = GetWords.GetWord("new_beta.txt");
-//Console.WriteLine($"[Binary] Amount of beta data: {Algoritme.Binary(beta).Count}");
-//Console.WriteLine($"[Word] Amount of beta data: {Algoritme.NotOptimised(beta).Count}");
-//string[] alpha = GetWords.GetWord("words_alpha.txt");
-//Console.WriteLine($"[Binary] Amount of beta data: {Algoritme.Binary(alpha).Count}");
-//Console.WriteLine($"[Word] Amount of beta data: {Algoritme.NotOptimised(alpha).Count}");
-
 var stopWatch = new System.Diagnostics.Stopwatch();
 stopWatch.Start();
 // run self call function
-List<string> combinations = new();
+//List<string> combinations = new();
 //Dictionary<int, string> beta = GetWords.GetWordBinary("new_beta.txt");
 Dictionary<int, string> alpha = GetWords.GetWordBinary("words_alpha.txt");
 
 int[] bitwords = new int[alpha.Count];
 alpha.Keys.CopyTo(bitwords, 0);
-await MultiTheardBinaryv2(0, 0, Array.Empty<int>());
-Console.WriteLine($"[Binary] Amount of beta data: {combinations.Count}");
+MultiThreadBinaryParallel();
+
 stopWatch.Stop();
 Console.WriteLine(stopWatch.Elapsed.ToString());
-//Console.WriteLine($"[Binary] Amount of beta data: {combinations.Count}");
 
-//Console.WriteLine($"[Word] Amount of beta data: {Algoritme.Binaryv2(0, 0, beta)}");
-
-static void Binaryv2(int startIndex, int usedBits, int[] combination, List<string> combinations, int[] bitwords)
+void FirstMultiTheardBinary(int startIndex, int[] words, int usedBits, int[] combination, List<string> combinations)
 {
-    if (combination.Length == 5)
-    {
-        combinations.Add($"{combination[0]} {combination[1]} {combination[2]} {combination[3]} {combination[4]}");
-        Console.WriteLine($"{combination[0]} {combination[1]} {combination[2]} {combination[3]} {combination[4]}");
-        return;
-    }
+    usedBits = bitwords[startIndex];
+    combination[0] = usedBits;
 
-    for (int i = startIndex; i < bitwords.Length; i++)
+    for (int i = startIndex; i < words.Length; i++)
     {
-        if ((usedBits & bitwords[i]) == 0)
+        if ((usedBits & words[i]) == 0)
         {
             int[] newCombination = new int[combination.Length + 1];
             combination.CopyTo(newCombination, 0);
             newCombination[combination.Length] = bitwords[i];
-            Binaryv2(i + 1, usedBits | bitwords[i], newCombination, combinations, bitwords);
+            int[] newWords = words.Skip(i + 1).ToArray();
+            newWords = newWords.Where(x => (x & usedBits) == 0).ToArray();
+            MultiTheardBinary(newWords, usedBits | words[i], newCombination, combinations);
         }
     }
+    return;
 }
 
-async Task MultiTheardBinaryv2(int startIndex, int usedBits, int[] combination)
+void MultiTheardBinary(int[] words, int usedBits, int[] combination, List<string> combinations)
 {
     if (combination.Length == 5)
     {
         string result = $"{combination[0]} {combination[1]} {combination[2]} {combination[3]} {combination[4]}";
-        await Task.Run(() =>
-        {
-            lock (combinations)
-            {
-                combinations.Add(result);
-                Console.WriteLine(result);
-            }
-        });
+        combinations.Add(result);
+        Console.WriteLine(result);
+        Console.WriteLine($"{alpha[combination[0]]} {alpha[combination[1]]} {alpha[combination[2]]} {alpha[combination[3]]} {alpha[combination[4]]}");
         return;
     }
 
-    var tasks = new List<Task>();
-    //var options = new ParallelOptions { MaxDegreeOfParallelism = degreeOfParallelism };
-    //Parallel.For(startIndex, bitwords.Length, options, i =>
-    //{
-    //    if ((usedBits & bitwords[i]) == 0)
-    //    {
-    //        int[] newCombination = new int[combination.Length + 1];
-    //        combination.CopyTo(newCombination, 0);
-    //        newCombination[combination.Length] = bitwords[i];
-    //        tasks.Add(MultiTheardBinaryv2(i + 1, usedBits | bitwords[i], newCombination, degreeOfParallelism));
-    //    }
-    //});
-    for (int i = startIndex; i < bitwords.Length; i++)
+
+    for (int i = 0; i < words.Length; i++)
     {
-        if ((usedBits & bitwords[i]) == 0)
+        if ((usedBits & words[i]) == 0)
         {
             int[] newCombination = new int[combination.Length + 1];
             combination.CopyTo(newCombination, 0);
             newCombination[combination.Length] = bitwords[i];
-            tasks.Add(MultiTheardBinaryv2(i + 1, usedBits | bitwords[i], newCombination));
+            int[] newWords = words.Skip(i + 1).ToArray();
+            newWords = newWords.Where(x => (x & usedBits) == 0).ToArray();
+            MultiTheardBinary(newWords, usedBits | words[i], newCombination, combinations);
         }
     }
-    await Task.WhenAll(tasks);
+    return;
+}
+
+void MultiThreadBinaryParallel()
+{
+    List<string> combinations = new();
+    //var tasks = new List<Task>();
+    var op = new ParallelOptions
+    {
+        MaxDegreeOfParallelism = 1
+    };
+    var t = Parallel.For(0, bitwords.Length, i =>
+    {
+        FirstMultiTheardBinary(i, bitwords, 0, new int[1], combinations);
+    });
+
+    Console.WriteLine($"[Binary] Amount of beta data: {combinations.Count}");
 }
